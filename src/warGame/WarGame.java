54ace26093a10b3ad79918d2f6e2e20c536203c3
Clint Chenault <clintchenault@hotmail.com>
2017-10-04 18:55:11 -0500

    Add files via upload

--- src/warGame/WarGame.java
+++ src/warGame/WarGame.java
@@ -10,7 +10,7 @@ import deck.VALUE;
 public class WarGame {
 
 	public static void main(String[] args) {
-		Intro();
+		//Intro();
 		Deck deck = new Deck();
 		deck.NormalDeck();
 		deck.Shuffle();
