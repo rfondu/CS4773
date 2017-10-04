19c96456663664dff98ee4171a7a2254be36fa81
Clint Chenault <clintchenault@hotmail.com>
2017-10-03 21:42:14 -0500

    Add files via upload
    Gloria's commit

--- src/Deck.java
+++ src/Deck.java
@@ -1,15 +1,20 @@
+package warGame;
+
 import java.util.ArrayList;
 
 public class Deck {
 	
 	public Deck() {
-		ArrayList cardDeck = new ArrayList();
+		ArrayList<Card> cardDeck = new ArrayList<Card>();
 		
-		for(int i = 0; i <= 4; i++) {
-			for(int j = 0; j <= 13; j++) {
-				Card card = new Card (VALUE.values(j), SUIT.values(i));
-				cardDeck.add(card);				
+		for(SUIT suit: SUIT.values()) {
+			for(VALUE value: VALUE.values()) {
+				cardDeck.add(new Card(value, suit));				
 			}
+		}
+		// TEMP just to see what's in the arraylist
+		for(Card c: cardDeck) {	
+			System.out.println("Suit " + c.getSuit() + " value " + c.getValue() + "\n");
 		}
 	}
 
--- src/WarGame.java
+++ src/WarGame.java
@@ -1,13 +1,19 @@
+package warGame;
 
 public class WarGame {
 
 	public static void main(String[] args) {
 		//new Intro();  // uncomment this before turn in
 		//Setup blah = new Setup();  // also uncomment this before turn in
-		Card test = new Card(VALUE.ACE, SUIT.SPADES);
+		//Card test = new Card(VALUE.ACE, SUIT.SPADES);
 		
-		System.out.println("Your card is " + test.getValue() + 
+	/*	System.out.println("Your card is " + test.getValue() + 
 				" of " + test.getSuit() + " and is worth " 
 				+ test.getValue().getCardValue());
+		ArrayList<Card> cardDeck = new ArrayList<Card>(); */
+		
+		Deck deck = new Deck();
+		
 	}
+
 }
