5a2361c6c6e58aa9354facf058db218b65ae6d58
Gloria Garza <gloriagza21@gmail.com>
2017-10-05 15:46:02 -0500

    added another print statement

--- src/warGame/Output.java
+++ src/warGame/Output.java
@@ -7,23 +7,23 @@ public class Output {
 		
 	Player name;
 	
-	public void PlayerWon(Player player){
+	public void RoundWinner(Player player){
 		System.out.println(name.getName() + " wins the round\n");
 	}
 	
-	public void PlayerCard(Player player, Card card){
+	public void CardPlayed(Player player, Card card){
 		System.out.println(name.getName() + " plays " + card.getValue() + " of " + card.getSuit() + " as up card \n");
 	}
 	
-	public void PlayerScore(Player players){
+	public void PlayersScore(Player players){
 		// this is assuming an arraylist of players will be passed 
 		// need to either pass the score or use array.length to get the score
 		// I know it's not correct but the logic is there just need to figure out what will be passed
 		
 		System.out.println("Score is "); 
 		String fencePost = ",";
-		for(Player p: Players) {
-			if(p == Players.length){
+		for(Player p: players) {
+			if(p == players.length()){
 				fencePost = " ";
 			}
 			System.out.println(p.name.getName() + " " + score + fencePost + " ");
@@ -32,10 +32,14 @@ public class Output {
 	}
 	
 	public void War(){	
-		System.out.println("WAR!\n");
+		System.out.println("WAR !\n");
 	}
 	
-	public void WinGame(Player player){	
+	public void GameWinner(Player player){	
 		System.out.println(name.getName() + " wins the game!\n");
+	}
+	
+	public void TieGame(){	
+		System.out.println("Tie game!\n");
 	}
 }
