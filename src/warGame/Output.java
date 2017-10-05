fe715e697feba23ded8e30c29dbb4c7ccac9909d
Clint Chenault <clintchenault@hotmail.com>
2017-10-05 18:08:58 -0500

    Modified Output

--- src/warGame/Output.java
+++ src/warGame/Output.java
@@ -1,45 +1,50 @@
 package warGame;
 
+import java.util.ArrayList;
+
 import deck.Card;
 import player.Player;
+import player.PlayerPoints;
 
 public class Output {
+	
+	public static void cardPlayedPrint(ArrayList<Player> players, ArrayList<Card> inPlay){
+		int lastCardPlayed = inPlay.size();
 		
-	Player name;
-	
-	public void RoundWinner(Player player){
-		System.out.println(name.getName() + " wins the round\n");
+		System.out.println(players.get(0).getName() + " plays " + inPlay.get(lastCardPlayed-1).getValue() + " of " + inPlay.get(lastCardPlayed-2).getSuit() + " as up card ");
+		System.out.println(players.get(1).getName() + " plays " + inPlay.get(lastCardPlayed).getValue() + " of " + inPlay.get(lastCardPlayed-1).getSuit() + " as up card ");
 	}
 	
-	public void CardPlayed(Player player, Card card){
-		System.out.println(name.getName() + " plays " + card.getValue() + " of " + card.getSuit() + " as up card \n");
-	}
-	
-	public void PlayersScore(Player players){
-		// this is assuming an arraylist of players will be passed 
+	public static void playersScorePrint(ArrayList<Player> players){
 		// need to either pass the score or use array.length to get the score
 		// I know it's not correct but the logic is there just need to figure out what will be passed
 		
 		System.out.println("Score is "); 
 		String fencePost = ",";
+		int i = 0;
 		for(Player p: players) {
-			if(p == players.length()){
+			if(i == players.size()){
 				fencePost = " ";
 			}
-			System.out.println(p.name.getName() + " " + score + fencePost + " ");
+			System.out.println(p.getName() + " " + p.getPoints() + fencePost + " ");  // **** How are we going to call points?  PlayerPoints array holds it but we are sending player
+			i++;																	 //      we may need to delete PlayerPoints and add its functionality to Player.java.  Thoughts? ****	
 		}
 		System.out.println("\n");
 	}
 	
-	public void War(){	
+	public static void warPrint(){	
 		System.out.println("WAR !\n");
 	}
 	
-	public void GameWinner(Player player){	
-		System.out.println(name.getName() + " wins the game!\n");
+	public static void roundWinnerPrint(String winnerName){
+		System.out.println(winnerName + " wins the round\n");
 	}
 	
-	public void TieGame(){	
+	public void gameWinnerPrint(String winnerName){	
+		System.out.println(winnerName + " wins the game!\n");
+	}
+	
+	public void tieGamePrint(){	
 		System.out.println("Tie game!\n");
 	}
 }
