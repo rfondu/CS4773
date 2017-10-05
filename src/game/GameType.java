028a1c9adbcfb3ad107417d3965dc63d016b6537
Gloria Garza <gloriagza21@gmail.com>
2017-10-04 23:29:04 -0500

    output methods there, need to figure out parms

--- src/game/GameType.java
+++ src/game/GameType.java
@@ -1,32 +1,32 @@
 package game;
 
 import deck.Card;
-import deck.SUIT;
-import deck.VALUE;
+import player.Player;
 
 public class GameType {
 
 	public void Round (Player player1, Player player2) {
-		PlayCards(player1.drawCard, player2.drawCard);	
+		PlayCards(player1.drawCard(), player2.drawCard());	
 	}
 	
 	public void PlayCards(Card player1Card, Card player2Card) {
+		warGame.Output.PlayerCard(player1, player1Card);
+		warGame.Output.PlayerCard(player2, player2Card);
 		// Call Print class to say what cards are being played by players
 		CompareCards(player1Card, player2Card);
 	}
 	
 	public void CompareCards (Card player1Card, Card player2Card) {		
 		if(player1Card.getValue().getCardValue() > player2Card.getValue().getCardValue()) {
-			System.out.println("WE GOT HERE PLAYER 1 WON!");
+			warGame.Output.PlayerWon(player1));
 			// Count size of in play array and assign points to player 1 int scoreValue OR player 1 unused hand array
 			RoundEnd();
 		} else if (player1Card.getValue().getCardValue() < player2Card.getValue().getCardValue()) {
-			System.out.println("WE GOT HERE PLAYER 2 WON!");
+			warGame.Output.PlayerWon(player2));
 			// Count size of in play array and assign points to player 2 int scoreValue OR player 2 unused hand array
 			RoundEnd();
 		} else {
-			System.out.println("WAR!");
-			// Print class to say TIE and WAR!
+			warGame.Output.War();
 			// Place one down card for each player and call PlayCards() to get another up card for each player
 		}
 	}
@@ -36,7 +36,7 @@ public class GameType {
 	}
 	
 	public void RoundEnd() {
-		// Print Class to say what player won!
+		warGame.Output.PlayeScore(player1, player2, score1, score2)
 	}
 
 }
